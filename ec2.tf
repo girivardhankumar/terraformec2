9b04f5ef6a5feb8dfca781a73396494895ee89de
girivardhankumar <47144113+girivardhankumar@users.noreply.github.com>
2019-04-16 14:37:11 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,33 +1,8 @@
 provider "aws" {
   region = "eu-west-2"
-  access_key = "AKIAIKFKPR34MH6ECILQ"
-  secret_key = "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"
-}
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"]
 }
 
 resource "aws_instance" "web" {
-  ami           = "${data.aws_ami.ubuntu.id}"
+  ami           = "ami-0019ef04ac50be30f"
   instance_type = "t2.micro"
-
-  tags {
-    Name = "HelloWorld"
   }
-}
-output "ip"{
-value= "${aws_instance.web.public_ip}"
-}
