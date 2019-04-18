5c68623e4f0580fca307fdf57f5e28606027c843
girivardhankumar <47144113+girivardhankumar@users.noreply.github.com>
2019-04-18 13:13:53 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,10 +2,14 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-resource "aws_instance" "web" {
-  ami           = "ami-0ec1ad91f200c15a8"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "LT_AMI"
-  }
-  }
+resource "aws_instance" "LT_Web_server" {
+
+    ami    = "ami-840112e4"
+
+    instance_type = "t2.micro"
+
+       tags  {
+
+          Name = "LT_Web_server"
+
+    }
